a6a56f565b6b7b864350cd62db11d708accec886
Ashton Fei <yunjia.fei@gmail.com>
2022-09-23 01:35:25 +0800

    feat: update theme color

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,5 +5,33 @@ import "vuetify/styles";
 // Vuetify
 import { createVuetify } from "vuetify";
 
-export default createVuetify();
+const light = {
+	dark: false,
+	colors: {
+		background: "#FEFEFE",
+		surface: "#FEFEFE",
+		primary: "#5384ED",
+		secondary: "#03DAC6",
+		error: "#D85040",
+		info: "#2196F3",
+		success: "#34A853",
+		warning: "#F1BF42",
+	}
+}
+
+const dark = {
+	dark: true,
+}
+
+const options = {
+	theme: {
+		defaultTheme: "light",
+		themes: {
+			light,
+			dark,
+		}
+	}
+}
+
+export default createVuetify(options);
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
