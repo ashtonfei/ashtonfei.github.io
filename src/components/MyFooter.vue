df5184566a23e15e574b47378c99e6ff7cc7bf5f
Ashton Fei <yunjia.fei@gmail.com>
2022-09-24 20:16:42 +0800

    fix: remove .DS_Store and add it to gitignore

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,20 +1,20 @@
 <template>
-  <v-footer app class="pa-0">
-    <v-card
-      elevation="0"
-      rounded="0"
-      width="100%"
-      class="bg-primary text-center"
-    >
-      <v-card-text class="text-white py-1">
-        <small>Automate the Boring</small>
-      </v-card-text>
-      <v-card-text class="text-white py-1">
-        {{ new Date().getFullYear() }} &copy; <strong>Ashton Fei</strong>
-      </v-card-text>
-    </v-card>
-  </v-footer>
+	<v-footer app class="pa-0">
+		<v-card
+			elevation="0"
+			rounded="0"
+			width="100%"
+			class="bg-primary text-center"
+		>
+			<v-card-text class="text-white py-1">
+				<small>Automate the Boring</small>
+			</v-card-text>
+			<v-card-text class="text-white py-1">
+				{{ new Date().getFullYear() }} &copy;
+				<strong>Ashton Fei</strong>
+			</v-card-text>
+		</v-card>
+	</v-footer>
 </template>
 
-<script setup>
-</script>
+<script setup></script>
